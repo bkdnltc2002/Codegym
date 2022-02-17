866f947100a8b4c9d69a801a318e0122f1bfe5ba
bkdnltc2002 <95325717+bkdnltc2002@users.noreply.github.com>
2022-02-17 20:44:30 +0700

    Add files via upload

--- Dictionary.py
+++ Dictionary.py
@@ -1,10 +1,10 @@
-def findWord(dict,input_word):
-        if input_word in dict:
-            print(dict[input_word])
-        else:
-            print("Your word is unvailable!")
+def findWord(dict, input_word):
+    if input_word in dict:
+        print(dict[input_word])
+    else:
+        print("Your word is unvailable!")
 
 
-dict={"Hello":"Xin Chao","Bye":"Tam Biet"}
-input_word=input("Type something to translate: ")
-findWord(dict,input_word)
+dict = {"Hello": "Xin Chao", "Bye": "Tam Biet", "dog":"cho","cat":"meo","rhino":"te giac","intention":"du dinh"}
+input_word = input("Type something to translate: ")
+findWord(dict, input_word)
