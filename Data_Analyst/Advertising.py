ced944b11ebab987d2b37c864fc818db79da7154
bkdnltc2002 <95325717+bkdnltc2002@users.noreply.github.com>
2022-06-16 16:15:57 +0700

    hoi quy tuyen tinh

--- Data_Analyst/Advertising.py
+++ Data_Analyst/Advertising.py
@@ -13,7 +13,8 @@ from sklearn.metrics import r2_score, mean_squared_error
 data = pd.read_csv("Data_Analyst\Advertising.csv")
 
 # Mô hình hồi quy tuyến tính đơn biến
-X = data['TV'].values.reshape(-1, 1)
+# X = data['TV'].values.reshape(-1, 1)
+X = data[['TV', 'Newspaper', 'Radio']].sum(axis=1).values.reshape(-1, 1)
 y = data['Sales'].values.reshape(-1, 1)
 # chia bộ dữ liệu làm 2 tập train và test theo tỉ lệ 80% train, 20% test
 X_train, X_test, y_train, y_test = train_test_split(
@@ -28,6 +29,6 @@ print("Coefficients:", regressor.coef_)
 y_pred = regressor.predict(X_test)
 
 # Đánh giá mô hình qua R2-score và mean_squrd_error
-score=r2_score(y_test,y_pred)
-print('R2-score is ',score)
-print('Mean_sqrd_error is==',mean_squared_error(y_test,y_pred))
+score = r2_score(y_test, y_pred)
+print('R2-score is ', score)
+print('Mean_sqrd_error is==', mean_squared_error(y_test, y_pred))
